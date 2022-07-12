0da196bcbee06a78478aae51c5ac39b89bfc8987
journiyoon <journiyoon@gmail.com>
2022-07-12 15:17:40 +0900

    testttttt

--- indentTest2.js
+++ indentTest2.js
@@ -1,4 +1,3 @@
-// 풀이 1
 function solution(nums) {
 	let answer = 0;
 	let setArray = [...new Set(nums)];
@@ -8,10 +7,4 @@ function solution(nums) {
 		answer = setArray.length;
 	}
 	return answer;
-}
-
-// 풀이 2
-function solution(nums) {
-	let setArray = [...new Set(nums)];
-	return nums.length / 2 > setArray.length ? setArray.length : nums.length / 2;
 }
