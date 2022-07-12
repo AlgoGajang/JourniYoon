0a28f4d28e2d32183bd454d345098983f106d0c3
journiyoon <journiyoon@gmail.com>
2022-07-12 14:53:24 +0900

    test: indent3

--- indentTest2.js
+++ indentTest2.js
@@ -8,3 +8,11 @@ function solution() {
 		console.log(i, j);
 	}
 }
+function solution() {
+	let array = new Array();
+	array.fill(0, 0, 5);
+	array.forEach((el) => console.log(el + 1));
+}
+if (array.length > 10) {
+	console.log(true);
+}
