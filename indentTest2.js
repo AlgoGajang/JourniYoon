3a1a11ef5482a518c900447b4c8e8fa6403e80dd
journiyoon <journiyoon@gmail.com>
2022-07-12 15:20:51 +0900

    test: indent test in webstorm

--- indentTest2.js
+++ indentTest2.js
@@ -8,3 +8,13 @@ function solution(nums) {
 	}
 	return answer;
 }
+function solution(nums) {
+	let answer = 0;
+	let setArray = [...new Set(nums)];
+	if (nums.length / 2 <= setArray.length) {
+		answer = nums.length / 2;
+	} else {
+		answer = setArray.length;
+	}
+	return answer;
+}
