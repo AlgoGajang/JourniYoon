31510348709140325ac923d953d424e7fff9e93b
journiyoon <journiyoon@gmail.com>
2022-07-19 00:07:37 +0900

    perf: reduce loop / [PGS] 카펫 / Level 2/ 60분

--- brute-force/carpet.js
+++ brute-force/carpet.js
@@ -1,22 +1,17 @@
 function solution(brown, yellow) {
-  var answer = [];
-  // yellow의 약수 배열을 찾아준다.
-  let divisor = [];
-  let i = 1;
+  let i = 1; // 1부터 시작해서 yellow의 약수를 찾는다.
   while (i < yellow + 1) {
-    if (yellow % i === 0) divisor.push(i);
+    if (yellow % i === 0) {
+      // yellow의 약수인 경우에 계산이 가능하다.
+      // 약수의 짝을 찾는다. (i가 1부터 시작하므로 'i <= j' 가 된다.
+      // 문제의 조건에서 '세로길이 < 가로길이'로 명시되어 있기 때문에 'j'가 가로길이가 된다.
+      let j = yellow / i;
+      if ((i + j + 2) * 2 === brown) {
+        // 위에서 찾은 yellow를 감싸는 카펫의 개수가 brown 개수와 일치하는 경우를 찾는다.
+        return [j + 2, i + 2];
+      }
+    }
     i++;
     continue;
   }
-  // 약수 배열을 돌며 brown 격자 개수에 해당하는 값을 찾는다.
-  for (
-    let i = 0, j = divisor.length - 1;
-    i < Math.ceil(divisor.length / 2);
-    i++, j--
-  ) {
-    if ((divisor[i] + divisor[j] + 2) * 2 === brown) {
-      answer.push(divisor[j] + 2, divisor[i] + 2); // 더 긴 쪽을 가로 길이로 넣어준다.
-    }
-  }
-  return answer;
 }
