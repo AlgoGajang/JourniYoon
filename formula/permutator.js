21d9b1e8040c6af295162cd5e89f2844c24c078e
journiyoon <journiyoon@gmail.com>
2022-07-27 17:14:36 +0900

    chore: repath

---
+++ formula/permutator.js
@@ -0,0 +1,24 @@
+let array = Array.from({ length: 3 }, (v, i) => i + 1);
+
+const permutator = (inputArr) => {
+  let result = [];
+
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  };
+
+  permute(inputArr);
+
+  return result;
+};
+
+let output = permutator(array);
+console.log(output);
