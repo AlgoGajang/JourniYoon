3c59f73f4dad5ca84537a58c473d901618dab7d9
journiyoon <journiyoon@gmail.com>
2022-08-04 23:45:53 +0900

    [LTC] Mirror Reflection / Medium / 60분(R)

---
+++ leetcode/858.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} p
+ * @param {number} q
+ * @return {number}
+ */
+const mirrorReflection = function (p, q) {
+  while (p % 2 === 0 && q % 2 === 0) {
+    p /= 2;
+    q /= 2;
+  }
+  if (p % 2 === 1 && q % 2 === 0) return 0;
+  if (p % 2 === 1 && q % 2 === 1) return 1;
+  else return 2;
+};
