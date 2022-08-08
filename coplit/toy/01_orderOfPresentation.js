2442b5b7396da17b23040776ed3ba09c4e9c6e45
journiyoon <journiyoon@gmail.com>
2022-08-08 17:24:34 +0900

    permutation & factorial / 1일(R)

---
+++ coplit/toy/01_orderOfPresentation.js
@@ -0,0 +1,23 @@
+function orderOfPresentation(N, K) {
+  let assist = Array.from({ length: N }, (v, i) => i + 1);
+  let answer = 0;
+  const factorial = (n) => {
+    let amount = 1;
+    if (n <= 1) return n;
+    return n * factorial(n - 1);
+  };
+  for (let i = 0; i < N; i++) {
+    let counter = 0;
+    for (const elem of assist) {
+      if (elem < K[i]) {
+        counter++;
+      }
+    }
+    assist.splice(assist.indexOf(K[i]), 1);
+    if (counter !== 0) {
+      answer += counter * factorial(N - i - 1);
+    }
+  }
+
+  return answer;
+}
